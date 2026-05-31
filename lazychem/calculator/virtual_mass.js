e40970102d9cdd2dd80bb64f805aab44a4f20edc
Nguyen Khang <khang.h.nguyen02@gmail.com>
2026-05-31 22:27:26 +0200

    virtual mass calculator now with multiple impurities

--- lazychem/calculator/virtual_mass.js
+++ lazychem/calculator/virtual_mass.js
@@ -1,5 +1,99 @@
 import { addGlobalEventListener } from "../../scripts/components/allComponents.js";
 
+
+const impurityList = JSON.parse(localStorage.getItem("impurityList")) || [{
+  integral: "",
+  protons: "1",
+  molecularWeight: "",
+}];
+
+function saveImpurityList() {
+  localStorage.setItem("impurityList", JSON.stringify(impurityList));
+};
+
+
+renderImpurityList();
+
+function renderImpurityList() {
+  let impurityListHTML = '';
+
+  impurityList.forEach((impurityObject, i) => {
+    const impurityWrapper = document.createElement("div");
+    // impurityWrapper.className = "impurity";
+
+    const number = i+1;
+
+    const html = `
+      <p>Impurity ${number}:</p>
+      <div>
+        <label for="impurity-integral-${number}">Integral:</label>
+        <input type="number" name="impurity-integral-${number}" id="impurity-integral-${number}" min="0" value="${impurityObject.integral}" required>
+      </div>
+      <div>
+        <label for="impurity-H-${number}">Number of protons (H):</label>
+        <input type="number" name="impurity-H-${number}" id="impurity-H-${number}" value="${impurityObject.protons}" min="1" required>
+      </div>
+      <div>
+        <label for="impurity-MW-${number}">Molecular weight:</label>
+        <input type="number" name="impurity-MW-${number}" id="impurity-MW-${number}" min="0" value="${impurityObject.molecularWeight}" required> g/mol
+      </div>
+
+      <button class="delete-button" data-delete-button="${i}">Delete</button>
+    `;
+    impurityWrapper.innerHTML = html;
+    
+    impurityListHTML += html;
+
+    addGlobalEventListener("input", `#impurity-integral-${number}`, e => {
+      impurityList[i].integral = e.target.value;
+      saveImpurityList();
+    });
+    addGlobalEventListener("input", `#impurity-H-${number}`, e => {
+      impurityList[i].protons = e.target.value;
+      saveImpurityList();
+    });
+    addGlobalEventListener("input", `#impurity-MW-${number}`, e => {
+      impurityList[i].molecularWeight = e.target.value;
+      saveImpurityList();
+    });
+  });
+  
+  document.querySelector("#impurities-display").innerHTML = impurityListHTML;
+
+  deleteImpurity();
+};
+
+function deleteImpurity() {
+  const deleteButtons = document.querySelectorAll(".delete-button");
+  // console.log(deleteButtons);
+  deleteButtons.forEach(button => {
+    button.addEventListener("click", () => {
+      const index = button.dataset.deleteButton;
+      impurityList.splice(index, 1);
+      saveImpurityList();
+      renderImpurityList();
+    });
+  });
+};
+
+function addImpurity() {
+  impurityList.push({
+    integral: "",
+    protons: "1",
+    molecularWeight: "",
+  });
+
+  saveImpurityList();
+  renderImpurityList();
+};
+
+addGlobalEventListener("click", "#add-impurity-button", () => {
+  addImpurity();
+});
+
+
+
+
 function calculateVirtualMass() {
   const totalMass = document.getElementById("total-mass").value;
   const productIntegral = document.getElementById("product-integral").value;
@@ -10,24 +104,48 @@ function calculateVirtualMass() {
     alert("Invalid numbers");
     return;
   };
-  
-  const impurityIntegral1 = document.getElementById("impurity-integral-1").value;
-  const impurityH1 = document.getElementById("impurity-H-1").value;
-  const impurityMW1 = document.getElementById("impurity-MW-1").value;
-  
-  if (!(Number(impurityIntegral1) && Number(impurityH1) && Number(impurityMW1))) {
-    alert("Invalid numbers");
-    return;
-  };
-  
   const productFraction = productIntegral/productH;
-  const impurity1Fraction = impurityIntegral1/impurityH1;
-  const purity = (productFraction / (productFraction + impurity1Fraction) * 100).toFixed(0);
+
+
+  let purityAcc = 0;
+  let impurityAcc = 0;
+  let ratio = productIntegral;
+  
+  impurityList.forEach((impurityObject, i) => {
+    const number = i + 1;
+    const impurityIntegral = impurityObject.integral;
+    const impurityH = impurityObject.protons;
+    const impurityMW = impurityObject.molecularWeight;
+
+    if (!(Number(impurityIntegral) && Number(impurityH) && Number(impurityMW))) {
+      alert("Invalid numbers");
+      return;
+    };
+
+    const impurityFraction = impurityIntegral / impurityH;
+    
+    purityAcc += impurityFraction;
+    impurityAcc += impurityFraction * impurityMW;
+    ratio += `:${impurityIntegral}`;
+  });
+
+  
+  // const impurityIntegral1 = document.getElementById("impurity-integral-1").value;
+  // const impurityH1 = document.getElementById("impurity-H-1").value;
+  // const impurityMW1 = document.getElementById("impurity-MW-1").value;
+  
+  // if (!(Number(impurityIntegral1) && Number(impurityH1) && Number(impurityMW1))) {
+  //   alert("Invalid numbers");
+  //   return;
+  // };
+  
+  // const impurity1Fraction = impurityIntegral1/impurityH1;
+  const purity = (productFraction / (productFraction + purityAcc) * 100).toFixed(0);
 
 
   const productMass = (totalMass * productFraction * productMW 
     / (productFraction * productMW 
-    + (impurity1Fraction) * impurityMW1)).toFixed(1);
+    + impurityAcc)).toFixed(1);
   
   const productMoles = (productMass / productMW).toPrecision(3);
 
@@ -120,15 +238,6 @@ function calculateVirtualMass() {
           
           +
           
-          <span class="frac">
-            <span>${impurityIntegral1}</span>
-            <span class="symbol">/</span>
-            <span class="bottom">${impurityH1}</span>
-          </span>
-          <span>&middot; ${impurityMW1}&nbsp;g/mol</span>
-
-          +
-
           ...
         </span>
       </span>
@@ -140,18 +249,17 @@ function calculateVirtualMass() {
   document.querySelector(".js-extra-field__virtual-mass").innerHTML = virtualMassHTML;
 
   document.getElementById("virtual-mass__copy").textContent = `
-    product (${virtualMassResultHTML}), ${purity}% purity (integrals: ${productIntegral}:${impurityIntegral1}).
+    product (${virtualMassResultHTML}), ${purity}% purity (integrals: ${ratio}).
   `.trim();
 
-  document.querySelectorAll(".virtual-mass__copy-text").forEach(copyButton => {
-    copyButton.addEventListener("click", () => {
-      const targetElement = document.querySelector(copyButton.dataset.copy);
-      targetElement.select(); // not really necessary
-      targetElement.setSelectionRange(0, 99999); // for mobile
-      navigator.clipboard.writeText(targetElement.textContent);
-      const tooltip = document.querySelector(".js-tooltiptext-virtual-mass");
-      tooltip.textContent = "Copied!";
-    });
+  const copyButton = document.querySelector(".virtual-mass__copy-text");
+  copyButton.addEventListener("click", () => {
+    const targetElement = document.querySelector(copyButton.dataset.copy);
+    targetElement.select(); // not really necessary
+    targetElement.setSelectionRange(0, 9999); // for mobile
+    navigator.clipboard.writeText(targetElement.textContent);
+    const tooltip = document.querySelector(".js-tooltiptext-virtual-mass");
+    tooltip.textContent = "Copied!";
   });
 };
 
@@ -164,97 +272,4 @@ addGlobalEventListener("keydown", "#virtual-mass input", e => {
   };
 });
 
-
-
-
-const impurityList = JSON.parse(localStorage.getItem("impurityList")) || [
-  {
-    integral: "",
-    protons: "1",
-    molecularWeight: "",
-  },
-];
-
-function saveImpurityList() {
-  localStorage.setItem("impurityList", JSON.stringify(impurityList));
-};
-
-renderImpurityList();
-
-function renderImpurityList() {
-  let impurityListHTML = '';
-
-  impurityList.forEach((impurityObject, i) => {
-    const impurityWrapper = document.createElement("div");
-    // impurityWrapper.className = "impurity";
-
-    const number = i+1;
-
-    const html = `
-      <p>Impurity ${number}:</p>
-      <div>
-        <label for="impurity-integral-${number}">Integral:</label>
-        <input type="number" name="impurity-integral-${number}" id="impurity-integral-${number}" min="0" value="${impurityObject.integral}" required>
-      </div>
-      <div>
-        <label for="impurity-H-${number}">Number of protons (H):</label>
-        <input type="number" name="impurity-H-${number}" id="impurity-H-${number}" value="${impurityObject.protons}" min="1" required>
-      </div>
-      <div>
-        <label for="impurity-MW-${number}">Molecular weight:</label>
-        <input type="number" name="impurity-MW-${number}" id="impurity-MW-${number}" min="0" value="${impurityObject.molecularWeight}" required> g/mol
-      </div>
-
-      <button class="delete-button" data-delete-button="${i}">Delete</button>
-    `;
-    impurityWrapper.innerHTML = html;
-    
-    impurityListHTML += html;
-
-    addGlobalEventListener("input", `#impurity-integral-${number}`, e => {
-      impurityList[i].integral = e.target.value;
-      saveImpurityList();
-    });
-    addGlobalEventListener("input", `#impurity-H-${number}`, e => {
-      impurityList[i].protons = e.target.value;
-      saveImpurityList();
-    });
-    addGlobalEventListener("input", `#impurity-MW-${number}`, e => {
-      impurityList[i].molecularWeight = e.target.value;
-      saveImpurityList();
-    });
-  });
-  
-  document.querySelector("#impurities-display").innerHTML = impurityListHTML;
-
-  deleteImpurity();
-};
-
-function deleteImpurity() {
-  const deleteButtons = document.querySelectorAll(".delete-button");
-  // console.log(deleteButtons);
-  deleteButtons.forEach(button => {
-    button.addEventListener("click", () => {
-      const index = button.dataset.deleteButton;
-      impurityList.splice(index, 1);
-      saveImpurityList();
-      renderImpurityList();
-    });
-  });
-};
-
-function addImpurity() {
-  impurityList.push({
-    integral: "",
-    protons: "1",
-    molecularWeight: "",
-  });
-
-  saveImpurityList();
-  renderImpurityList();
-};
-
-addGlobalEventListener("click", "#add-impurity-button", () => {
-  addImpurity();
-});
 
