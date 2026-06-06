dcde3d9836a392ba8efece9225d9c5c46a84a598
Nguyen Khang <khang.h.nguyen02@gmail.com>
2026-06-06 21:06:09 +0200

    Chemillust: unique active buttons

--- lazychem/chemillust/chemillust.js
+++ lazychem/chemillust/chemillust.js
@@ -1,8 +1,43 @@
+//@ts-check
 import { addGlobalEventListener } from "../../scripts/components/allComponents.js";
 
-addGlobalEventListener("click", "#toolbar--drawer", e => {
-  // const button = docu
-  
-  console.log(e);
-  console.log("button");
+
+/**
+ * Clicked button will be the active button.
+ * @param {object} currentButton - clicked button
+ */
+function setAction(currentButton) {
+  drawerActiveButton?.classList.remove("active");
+  currentButton.classList.add("active");
+  drawerActiveButton = currentButton;
+};
+
+
+const canvas = document.getElementById("canvas");
+
+/**
+ * @type {object}
+ */
+let drawerActiveButton = document.querySelector("#button--draw-atom");
+
+/**
+ * @type {object}
+*/
+const drawerButtons = document.querySelectorAll(".toolbar--drawer-button");
+
+drawerButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    setAction(button);
+  });
 });
+
+
+// addGlobalEventListener("click", "#button--draw-atom", e => {
+//   // const button = docu
+//   console.log(e);
+// });
+// addGlobalEventListener("click", "#button--draw-bond", e => {
+//   // const button = docu
+//   console.log(e);
+// });
+
